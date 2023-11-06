e83840d613d76fe2cf2c3e98d8d42af92e6a72b0
Svetlana Kulikova <1407kii@gmail.com>
2023-11-06 22:31:28 +0700

    Initial

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 140_792;
+        int bonusCost = 20;
+        int bonus = (ticketCost / bonusCost);
+
+        System.out.println(("Начислено бонусных миль ") + bonus);
+
+
+    }
+}
